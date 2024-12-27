65987c2b893b024c0aaca76d8a592fcf9150f9b7
MN Raza <mnraza94@gmail.com>
2024-12-27 07:46:50 +0530

    created a express server

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,13 @@
 import dotenv from "dotenv";  
 dotenv.config();  
 import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
