06f6eddd71091f888e02480f80c4a9bef2d940a0
MN Raza <mnraza94@gmail.com>
2024-12-27 08:26:44 +0530

    working on User Model for defining the fields required in User Model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 
 const userSchema = new mongoose.Schema({
     email: {
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        lowercase:true,
-        minLength:[6,"Email must be at least 6 characters long"],
-        maxLength:[50,"Email must not be longer than 50 characters"]
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: [6, "Email must be at least 6 characters long"],
+        maxLength: [50, "Email must not be longer than 50 characters"]
+    },
+    password: {
+        type: String,
     }
 
 })
+
+userSchema.statics.hasPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
