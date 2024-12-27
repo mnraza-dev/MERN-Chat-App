6840e4a6bea360b97efb3d4e6523d660ddc3ebea
MN Raza <mnraza94@gmail.com>
2024-12-27 07:52:21 +0530

    created server.js to listen the server

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,13 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+import http from "http";
+import app from "./app";
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
