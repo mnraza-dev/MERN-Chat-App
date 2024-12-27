8fe0a20e2729e587474236bce75d7afd9f765a2c
MN Raza <mnraza94@gmail.com>
2024-12-27 08:01:40 +0530

    made changes in package.json and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
+import dotenv from "dotenv";  
+dotenv.config();  
 import http from "http";
 import app from "./app";
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
